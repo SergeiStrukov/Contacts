a1251a54698f1931fbd3d56a007bf928bfa35c90
SergeiStrukov <serega.strukov21@gmail.com>
2024-08-14 01:33:30 +0400

    add Contact

--- src/main/java/com/demo/contacts/controllers/DemoController.java
+++ src/main/java/com/demo/contacts/controllers/DemoController.java
@@ -1,10 +1,7 @@
 package com.demo.contacts.controllers;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/demo")
@@ -13,6 +10,17 @@ public class DemoController {
     @GetMapping("/{id}")
     public ResponseEntity<String> getItemById(@PathVariable("id") String value) {
         String response = String.format("Передано значение: %s", value);
+        return ResponseEntity.ok(response);
+    }
+    @PostMapping("/postItem")
+    public ResponseEntity<String> postItemWithValue(@RequestBody String data) {
+        String response = String.format("postItemWithValue получил данные: %s", data);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> postItem(@RequestBody String data) {
+        String response = String.format("postItem получил данные: %s", data);
         return ResponseEntity.ok(response);
     }
 }
---
+++ src/main/java/com/demo/contacts/model/Contact.java
@@ -0,0 +1,11 @@
+package com.demo.contacts.model;
+
+import java.util.UUID;
+
+public class Contact {
+    private UUID id;
+    private String name;
+    private String email;
+
+    // + всё необходимое
+}
